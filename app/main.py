4dee58029a2f9310fcdd48bbeb531eeadc123eee
Olena Nosenko <nosenko.alena@gmail.com>
2026-08-20 13:15:11 +0300

    updated implementation for distance class task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
 
     def __init__(self, km: int | float) -> None:
@@ -9,22 +12,22 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance | int | float") -> "Distance":
+    def __add__(self, other: "Distance | int | float") -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: "Distance | int | float") -> "Distance":
+    def __iadd__(self, other: "Distance | int | float") -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: "Distance | int | float") -> "Distance":
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: "Distance | int | float") -> "Distance":
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: "Distance | int | float") -> bool:
