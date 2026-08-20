5ad151def12b2fdc1ad2c0c5329580660bbe2696
Olena Nosenko <nosenko.alena@gmail.com>
2026-08-20 13:26:52 +0300

    updated implementation for distance class task according to comments in PR

--- app/main.py
+++ app/main.py
@@ -17,11 +17,11 @@ class Distance:
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: "Distance | int | float") -> Distance:
+    def __iadd__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, Distance):
-            self.km += other.km
-        else:
-            self.km += other
+            other = other.km
+
+        self.km += other
         return self
 
     def __mul__(self, other: int | float) -> Distance:
